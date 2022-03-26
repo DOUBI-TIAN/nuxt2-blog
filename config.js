e8db0ee203fbd23f72900a4f7b4804da40cdafb0
逗比？天 <81632990+DOUBI-TIAN@users.noreply.github.com>
2022-03-26 22:46:04 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 export default {
   title: 'yun yu yuan',
-  githubName: 'yunyuyuan',
+  githubName: 'DOUBI-TIAN',
   githubRepo: 'nuxt2-blog',
   githubBranch: 'master',
   domain: 'https://blog.halberd.cn',
-  SEO_description: '云与原的博客，书写值得书写的东西',
-  SEO_keywords: 'yunyuyuan,yunyuyuan\'s blog,云与原,云与原的博客',
+  SEO_description: '逗比？天的博客，记录逗比生活每一天。',
+  SEO_keywords: 'yunyuyuan,yunyuyuan\'s blog,逗比？天,逗比？天的博客',
   CloudflareAnalyze: "92cdf13c9c964ad1ab71c638c6fde0fc"
 }
